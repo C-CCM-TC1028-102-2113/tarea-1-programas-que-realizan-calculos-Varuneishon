8489ffed9dcd045181a1f953aabf882eb6680ce0
Varuneishon <89056489+Varuneishon@users.noreply.github.com>
2021-08-18 23:13:56 -0500

    Update exercise.py

--- assignments/08Caracol/src/exercise.py
+++ assignments/08Caracol/src/exercise.py
@@ -1,5 +1,14 @@
 def main():
     #escribe tu código abajo de esta línea
+    import math
+print("Este programa calcula la distancia recorida por un caracol" )
+
+t = float(input("Dame los minutos : " ))
+
+v = 34.2
+d = v * t 
+
+print("Centímentros recorridos:" ,d)
     pass
 
 if __name__ == '__main__':
